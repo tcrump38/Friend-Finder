9c1011d5897d1d1d9d499977050ff84a64a19329
tcuddle <thcrump38@yahoo.com>
2019-01-16 12:34:53 -0600

    first push, workorking on styling

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+	name: "Mike McMillan",
+	photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+	name: "Marsha Linden",
+	photo: "https://images.pexels.com/photos/1583582/pexels-photo-1583582.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+	name: "Shelby Wright",
+	photo: "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+	name: "Ron Epstein",
+	photo: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+	name: "Joy Eberly",
+	photo: "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+	name: "Dan Pennington",
+	photo: "https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+	name: "Nicole Barber",
+	photo: "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+	scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
